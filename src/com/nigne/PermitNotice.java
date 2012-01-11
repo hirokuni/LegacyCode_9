fe550a4c4bbb79d1c5d57816feb21cdeebe79ee6
Hirokuni Kawasaki <hiro.vo.ov@gmail.com>
2012-01-12 07:04:45 +0900

    Singleton is difficult for test

---
+++ src/com/nigne/PermitNotice.java
@@ -0,0 +1,12 @@
+package com.nigne;
+
+public class PermitNotice {
+	
+	public PermitNotice(int first, String second) {
+		// TODO Auto-generated constructor stub
+	}
+
+	boolean isValid(){
+		return true;
+	}
+}
---
+++ test/src/com/nigne/AndroidTestCase.java
@@ -0,0 +1,9 @@
+package com.nigne;
+
+public class AndroidTestCase {
+  public void testCreate() throws Exception{
+	PermitNotice notice = new PermitNotice(0,"a");
+	//Want create facility in test code
+	Facility facility = new Facility(Facility.RESIDENCE, "b", notice);
+  }
+}
