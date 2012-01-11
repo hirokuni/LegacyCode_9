972ae9f906abef4683aeb0946e34be12025379b6
Hirokuni Kawasaki <hiro.vo.ov@gmail.com>
2012-01-12 07:48:03 +0900

    Singleton is difficult for test

--- test/src/com/nigne/AndroidTestCase.java
+++ test/src/com/nigne/AndroidTestCase.java
@@ -1,6 +1,15 @@
 package com.nigne;
 
 public class AndroidTestCase {
+  public void setup() {
+	  PermitRepository repository = new PermitRepository();
+	  //・・何かの処理
+	  
+	  //ここでset関数(setTestingInstance)を使ってpermitをrepositoryに登録する
+	  
+	  //・・何かの処理
+  }
+
   public void testCreate() throws Exception{
 	PermitNotice notice = new PermitNotice(0,"a");
 	//Want create facility in test code
