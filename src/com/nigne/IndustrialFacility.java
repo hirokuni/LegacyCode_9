d20050948207a4c166a7054edc9716af046b540f
Hirokuni Kawasaki <hiro.vo.ov@gmail.com>
2012-01-12 12:13:01 +0900

    2nd commit for 9.7

--- src/com/nigne/IndustrialFacility.java
+++ src/com/nigne/IndustrialFacility.java
@@ -1,21 +1,21 @@
 package com.nigne;
 
 public class IndustrialFacility extends Facility {
-  Permit basePermit;
-  
-  public IndustrialFacility(int facilityCode, String owner, OriginationPermit permit) throws PermitViolation{
-	  super();
-	  Permit associatedPermit = PermitRepository.getInstance().findAssociatedFromOrigination(permit);
-	  
-	  if (associatedPermit.isValid() && !permit.isValid()){
-		  basePermit = associatedPermit;
-	  }
-	  else if(!permit.isValid()){
-		  permit.validate();
-		  basePermit = permit;
-	  }
-	  else{
-		  throw new PermitViolation(permit);
-	  }
-  }
+	Permit basePermit;
+
+	public IndustrialFacility(int facilityCode, String owner,
+			OriginationPermit permit) throws PermitViolation {
+		super();
+		Permit associatedPermit = PermitRepository.getInstance()
+				.findAssociatedFromOrigination(permit);
+
+		if (associatedPermit.isValid() && !permit.isValid()) {
+			basePermit = associatedPermit;
+		} else if (!permit.isValid()) {
+			permit.validate();
+			basePermit = permit;
+		} else {
+			throw new PermitViolation(permit);
+		}
+	}
 }
