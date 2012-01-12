578ac21897f26120bcea06ee810ecd676933fd5d
Hirokuni Kawasaki <hiro.vo.ov@gmail.com>
2012-01-12 10:33:21 +0900

    Database connection

--- src/com/nigne/Facility.java
+++ src/com/nigne/Facility.java
@@ -47,34 +47,3 @@ class Permit {
 
 }
 
-class PermitRepository {
-	// singletoneパターン。グローバル変数を使うための仕組みの一つ。
-	// テストにおいて大変なのは、そのクラスがどのグローバル変数を使っているかを
-	// 理解した上で、それらをテストのために適切な状態に設定しなければならないこと。
-	// そして、設定する状態がそれぞれ異なる場合は、テストケース毎に状態の設定を
-	// 行う必要がある。これはかなり退屈。
-
-	// Singletonの趣旨はアプリケーションの中でSingletonのインスタンスを一つ以上作れないようにすることであり、
-	// 本番コード（製品コード）ではOKかもしれない。しかしテストにおいて各テストは独立すべきで、
-	// このSingletonをそれぞれのテストで適切な状態に設定する必要がある。
-	private static PermitRepository instance;
-
-	private PermitRepository() {
-	}
-
-	// テスト用のPermitRepositoryインスタンスを作って、それをセットできるようになった。
-	public static void setTestingInstance(PermitRepository newInstance) {
-		instance = newInstance;
-	}
-
-	public static PermitRepository getInstance() {
-		if (instance == null) {
-			return instance = new PermitRepository();
-		}
-		return instance;
-	}
-
-	public Permit findAssociatedPermit(PermitNotice notice) {
-		return new Permit();//本書では処理が不明なので、適当なコードを追加
-	}
-}
